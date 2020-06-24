1257b9aebf1317a28e8324d60523d5126e7a17ba
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2020-06-24 16:57:59 -0700

    project-completed

---
+++ src/component/PhotoList.js
@@ -0,0 +1,25 @@
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function PhotoList() {
+    const [data, setData] = useState({})
+    useEffect(() => {
+        axios
+        .get('https://api.nasa.gov/planetary/apod?api_key=miI40eqMuxQHfzdZnREccFeTZflbRiOwARDCDhwI&date=2012-03-25')
+        .then(response => setData(response.data))
+    }, [])
+
+    console.log(data)
+    return (
+        <div className = 'wrapper'>
+            <h1>{data.title}</h1>
+            <h3>{data.date}</h3>
+            <img alt={data.title} src={data.url}/>
+            <h3>{data.copyright}</h3>
+            <p>{data.explanation}</p>
+        </div>
+    )
+}
+
+export default PhotoList;
